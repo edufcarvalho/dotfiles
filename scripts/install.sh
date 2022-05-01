3f639cf4b302121aa689d9b719fd2fca230d0f21
Eduardo Freitas de Carvalho <eduardofc@ic.ufrj.br>
2022-05-01 15:11:32 -0300

    Make install.sh smarter
    Now install.sh check if on arch before trying to install, check if
    zsh is installed before trying to turn it into the default shell and
    check if cinnamon is the default DE before dumping the dconf

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 MAIN_DIR="$(dirname "$SCRIPT_DIR")"
 DOTFILES_DIR="$MAIN_DIR/files/"
 BACKUPS_DIR="$MAIN_DIR/backups/"
+DEFAULT_DE="X-Cinnamon"
 
 # Delete existing dot files and folders
 sudo rm -rf $HOME/.gitconfig > /dev/null 2>&1
@@ -17,12 +18,21 @@ ln -sf $DOTFILES_DIR/.zsh_plugins.txt $HOME/.zsh_plugins.txt
 ln -sf $DOTFILES_DIR/.zshrc $HOME/.zshrc
 ln -sf $DOTFILES_DIR/.config $HOME/.config
 
-# Restore dconf backup
-dconf reset -f /
-dconf load / < $BACKUPS_DIR/backup.dconf
+# Restore dconf backup if on cinnamon
+if [ $XDG_CURRENT_DESKTOP == $DEFAULT_DE ]; then
+	dconf reset -f /
+	dconf load / < $BACKUPS_DIR/backup.dconf
+fi
 
-# Install all packages
-yay -S $(cat $BACKUPS_DIR/packages.txt)
+# Install all packages if on arch
+if [ -f "/etc/arch-release" ]; then
+	PACKAGE_LIST=$(comm -1 -3 --nocheck-order <(yay -Qe | awk '{print $1}') .dotfiles/backups/packages.txt)
+	yay -S $PACKAGE_LIST
+fi
 
-# Change default shell to zsh
-chsh -s $(which zsh)
+# Change default shell to zsh if installed
+if [ -f "/bin/zsh" ]; then
+	chsh -s $(which zsh)
+else
+	echo "Install zsh and run the script again"
+fi
